c81daa1a1310b8663f5ababf539b9a9191d48737
Shanda Lau <shandalaulv@gmail.com>
2019-11-16 00:23:06 +0800

    Build detection module

--- detection/thresh.py
+++ detection/thresh.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import cv2
-import os.path as osp
-import numpy as np 
+import numpy as np
+from log import logger
 
 path = '/home/cys/Codes/DolphinDetection'
 
@@ -12,25 +12,25 @@ canny_hth = 125
 kernel_size = np.ones((3, 3), np.uint8)
 
 
-def process_img(img):
+def adaptive_thresh(img):
     img = img[370:1080, 0:1980]
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     blur = cv2.GaussianBlur(gray, (blur_ksize, blur_ksize), 1)
     th_atm = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 21, 20)
     # ret_otsu, th_otsu = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     edges = cv2.Canny(th_atm, canny_lth, canny_hth)
-    return edges
+    dilation = cv2.dilate(edges, kernel_size)
+    return dilation
 
-def main():
-    init_path = osp.join(path, 'Demo/picts/test_2.jpg')
-    save_path = osp.join(path, 'Demo/picts/2.jpg')
-
-    img = cv2.imread(init_path)
-    result = process_img(img)
-    dilation = cv2.dilate(result, kernel_size)
-    cv2.imwrite(save_path, dilation)
-
-if __name__ == '__main__':
-    main()
-    print("Done!")
-    
+# def main():
+#     init_path = osp.join(path, 'Demo/picts/test_2.jpg')
+#     save_path = osp.join(path, 'Demo/picts/2.jpg')
+#
+#     img = cv2.imread(init_path)
+#     result = adaptive_thresh(img)
+#     # cv2.imwrite(save_path, dilation)
+#
+#
+# if __name__ == '__main__':
+#     main()
+#     print("Done!")
