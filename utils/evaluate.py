94965b45d3467c9eb4758d357da7c6c7d9cafe85
ChanSiYuan <46964494+ChanSiYuan@users.noreply.github.com>
2019-11-23 18:15:33 +0800

    crop the image and modified evluate

--- utils/evaluate.py
+++ utils/evaluate.py
@@ -15,35 +15,45 @@ canny_lth = 75
 canny_hth = 125
 kernel_size = np.ones((3, 3), np.uint8)
 temperature = 300
+crop_temperature = 315
 
 
 def evaluate_label(path):
     img_paths = glob.glob(osp.join(path, '*.png'))
     label_keys, label_vals = I.load_label_config(LABEL_SAVE_PATH / 'samples.json')
 
+    origin = cv2.imread(img_paths[0])
+    size = origin.shape
+
     idx, num= -1, 0
     for img_path in img_paths:
         idx += 1
         origin = cv2.imread(img_path)
-        gray = cv2.cvtColor(origin, cv2.COLOR_RGB2GRAY)
+        crop = origin[crop_temperature:size[0], 0:size[1]]
+        # cv2.imshow("dila",crop)
+        # cv2.waitKey(0)
+        gray = cv2.cvtColor(crop, cv2.COLOR_RGB2GRAY)
         blur = cv2.GaussianBlur(gray, (blur_ksize, blur_ksize), 1)
         ada_thresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 21, 20)
         edges = cv2.Canny(ada_thresh, canny_lth, canny_hth)
         dilation = cv2.dilate(edges, kernel_size)
         # cv2.imshow("dila",dilation)
         # cv2.waitKey(0)
-        x, y = label_vals[idx].center[0], label_vals[idx].center[1]
+        x, y = label_vals[idx].center[0], label_vals[idx].center[1] - crop_temperature
         flag = 0
+        # contours, hierarchy = cv2.findContours(dilation, 1, 2)
+        # cnt = contours[0]
+        # M = cv2.moments(cnt)
+        # cx = int(M['m10']/M['m00'])
+        # cy = int(M['m01']/M['m00'])
         for i in range(temperature):
             for j in range(temperature):
-                if dilation[x - int(temperature / 2) + i, y - int(temperature / 2) + j] == 255:
+                if dilation[x - int(temperature / 2) - crop_temperature + i, y - int(temperature / 2) + j] == 255:
                     flag += 1
         if flag > 0:
             num += 1
 
-    origin = cv2.imread(img_paths[0])
-    size = origin.shape
-    all_area = size[0] * size[1]
+    all_area = size[0] * (size[1] - crop_temperature)
     estimate_area = temperature * temperature
     estimate_ratio = estimate_area / all_area
 
