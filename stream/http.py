2f9055d56912713f96dff6e390644afa5e217d2a
XiangDe <qq313700046@icloud.com>
2020-02-07 15:58:02 +0800

    Add HTTP server

---
+++ stream/http.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author: Shanda Lau 刘祥德
+@license: (C) Copyright 2019-now, Node Supply Chain Manager Corporation Limited.
+@contact: shandalaulv@gmail.com
+@software: 
+@file: http.py
+@time: 2/7/20 11:43 AM
+@version 1.0
+@desc:
+"""
+from flask import Flask, url_for, request, redirect
+from multiprocessing import Process
+
+app = Flask(__name__)
+
+
+class HttpServer(object):
+
+    def __init__(self, host_ip="127.0.0.1", host_port="8080", mode='dev', source_root=""):
+        self.host_ip = host_ip
+        self.host_port = host_port
+        self.mode = mode
+
+    @staticmethod
+    @app.route('/video/', methods=['GET'])
+    def video():
+        camera = request.args.get('camera')
+        date = request.args.get("date")
+        video_id = request.args.get("v_id")
+
+        url = "{}/{}/{}.mp4".format(camera, date, video_id)
+
+        return app.send_static_file(url)
+
+    def run(self):
+        if self.mode == 'dev':
+            Process(target=app.run, args=(self.host_ip, self.host_port, False,), daemon=True).start()
+        elif self.mode == 'test':
+            Process(target=app.run, args=(self.host_ip, self.host_port, False,), daemon=True).start()
+        elif self.mode == 'prod':
+            Process(target=app.run, args=(self.host_ip, self.host_port, True,), daemon=True).start()
+
+
+if __name__ == '__main__':
+    http = HttpServer("127.0.0.1", "8080")
+    http.run()
