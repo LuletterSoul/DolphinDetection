089936ae95dea55c970e4da5e0be0e8583c3ae3e
JinShiyin <shiyinjin@foxmail.com>
2020-03-15 22:25:20 +0800

    add post filter

--- detection/render.py
+++ detection/render.py
@@ -22,9 +22,13 @@ import os
 from config import SystemStatus
 # from .manager import DetectorController
 from stream.websocket import creat_packaged_msg_json
-from utils import logger, bbox_points, generate_time_stamp
+from utils import logger, bbox_points, generate_time_stamp, crop_by_se
 from stream.rtsp import FFMPEG_MP4Writer
 import traceback
+from config import VideoConfig
+from detection.params import DispatchBlock, DetectorParams
+from detection.detect_funcs import detect_based_task, detect_mask_task
+from pathlib import Path
 
 
 class DetectionStreamRender(object):
@@ -66,6 +70,7 @@ class DetectionStreamRender(object):
         self.status = Manager().Value('i', SystemStatus.RUNNING)
         self.notify_queue = notify_queue
         self.LOG_PREFIX = f'Video Stream Render [{self.cfg.index}]: '
+        self.post_filter = PostFilter(self.cfg)
 
     def listen(self):
         if self.quit.wait():
@@ -274,10 +279,11 @@ class DetectionStreamRender(object):
 
     def rect_render_task(self, current_idx, current_time, frame_cache, rect_cache, render_cache):
         start = time.time()
+        task_cnt = self.stream_cnt
         # raw_target = self.original_stream_path / (current_time + str(self.stream_cnt) + '_raw' + '.mp4')
-        target = self.rect_stream_path / (current_time + str(self.stream_cnt) + '.mp4')
+        target = self.rect_stream_path / (current_time + str(task_cnt) + '.mp4')
         logger.info(
-            f'Video Render [{self.index}]: Rect Render Task [{self.stream_cnt}]: Writing detection stream frame into: [{str(target)}]')
+            f'Video Render [{self.index}]: Rect Render Task [{task_cnt}]: Writing detection stream frame into: [{str(target)}]')
         # fourcc = cv2.VideoWriter_fourcc(*'avc1')
         # video_write = cv2.VideoWriter(str(raw_target), self.fourcc, 24.0, (self.cfg.shape[1], self.cfg.shape[0]), True)
         video_write = FFMPEG_MP4Writer(str(target), (self.cfg.shape[1], self.cfg.shape[0]), 25)
@@ -289,14 +295,14 @@ class DetectionStreamRender(object):
         # wait the futures frames is accessable
         if not self.next_prepare_event.is_set():
             logger.info(
-                f'Video Render [{self.index}]: Rect Render Task [{self.stream_cnt}] wait frames accessible....')
+                f'Video Render [{self.index}]: Rect Render Task [{task_cnt}] wait frames accessible....')
             start = time.time()
             # wait the future frames prepared,if ocurring time out, give up waits
             self.next_prepare_event.wait(30)
             logger.info(
                 f"Video Render [{self.cfg.index}]: Rect Render Task " +
-                f"[{self.stream_cnt}] wait [{round(time.time() - start, 2)}] seconds")
-            logger.info(f'Video Render [{self.index}]: Rect Render Task [{self.stream_cnt}] frames accessible...')
+                f"[{task_cnt}] wait [{round(time.time() - start, 2)}] seconds")
+            logger.info(f'Video Render [{self.index}]: Rect Render Task [{task_cnt}] frames accessible...')
 
         # if not self.started:
         #     return False
@@ -308,20 +314,22 @@ class DetectionStreamRender(object):
         video_write.release()
         # self.convert_byfile(str(raw_target), str(target))
         logger.info(
-            f'Video Render [{self.index}]: Rect Render Task [{self.stream_cnt}]: Consume [{round(time.time() - start, 2)}] ' +
+            f'Video Render [{self.index}]: Rect Render Task [{task_cnt}]: Consume [{round(time.time() - start, 2)}] ' +
             f'seconds.Done write detection stream frame into: [{str(target)}]')
-        msg_json = creat_packaged_msg_json(filename=str(target.name), path=str(target), cfg=self.cfg)
-        # if raw_target.exists():
-        #     raw_target.unlink()
-        self.msg_queue.put(msg_json)
-        logger.info(f'put packaged message in the msg_queue...')
+        if not self.post_filter.post_filter(str(target), task_cnt):
+            msg_json = creat_packaged_msg_json(filename=str(target.name), path=str(target), cfg=self.cfg)
+            # if raw_target.exists():
+            #     raw_target.unlink()
+            self.msg_queue.put(msg_json)
+            logger.info(f'put packaged message in the msg_queue...')
 
     def original_render_task(self, current_idx, current_time, frame_cache):
         start = time.time()
+        task_cnt = self.stream_cnt
         # raw_target = self.original_stream_path / (current_time + str(self.stream_cnt) + '_raw' + '.mp4')
-        target = self.original_stream_path / (current_time + str(self.stream_cnt) + '.mp4')
+        target = self.original_stream_path / (current_time + str(task_cnt) + '.mp4')
         logger.info(
-            f'Video Render [{self.index}]: Original Render Task [{self.stream_cnt}]: Writing detection stream frame into: [{str(target)}]')
+            f'Video Render [{self.index}]: Original Render Task [{task_cnt}]: Writing detection stream frame into: [{str(target)}]')
         # video_write = cv2.VideoWriter(str(raw_target), self.fourcc, 24.0, (self.cfg.shape[1], self.cfg.shape[0]), True)
         video_write = FFMPEG_MP4Writer(str(target), (self.cfg.shape[1], self.cfg.shape[0]), 25)
         # if not video_write.isOpened():
@@ -338,8 +346,8 @@ class DetectionStreamRender(object):
             # wait the future frames prepared,if ocurring time out, give up waits
             self.next_prepare_event.wait(30)
             logger.info(
-                f"Video Render [{self.cfg.index}]: Original Render Task [{self.stream_cnt}] wait [{time.time() - start}] seconds")
-            logger.info(f'Video Render [{self.index}]: Original Render Task [{self.stream_cnt}] frames accessible....')
+                f"Video Render [{self.cfg.index}]: Original Render Task [{task_cnt}] wait [{time.time() - start}] seconds")
+            logger.info(f'Video Render [{self.index}]: Original Render Task [{task_cnt}] frames accessible....')
         # logger.info('Render task Begin with frame [{}]'.format(next_cnt))
         # logger.info('After :[{}]'.format(render_cache.keys()))
         # if not self.started:
@@ -349,7 +357,7 @@ class DetectionStreamRender(object):
         video_write.release()
         # self.convert_byfile(str(raw_target), str(target))
         logger.info(
-            f'Video Render [{self.index}]: Original Render Task [{self.stream_cnt}]: ' +
+            f'Video Render [{self.index}]: Original Render Task [{task_cnt}]: ' +
             f'Consume [{round(time.time() - start, 2)}] seconds.Done write detection stream frame into: [{str(target)}]')
         # if raw_target.exists():
         #     raw_target.unlink()
@@ -382,3 +390,133 @@ class DetectionStreamRender(object):
     #     # print("Converting ", from_path)
     #     line = self.convert_avi_to_mp4(from_path, to_path)
     #     logger.info(line)
+
+
+class PostFilter(object):
+    def __init__(self, cfg: VideoConfig):
+        self.cfg = cfg
+        self.detect_params = self.set_detect_params()
+        self.speed_thresh_x = 20
+        self.speed_thresh_y = 20
+
+    def set_detect_params(self):
+        x_num = 1
+        y_num = 1
+        x_step = int(self.cfg.shape[1] / x_num)
+        y_step = int(self.cfg.shape[0] / y_num)
+
+        detect_params = []
+        for i in range(x_num):
+            for j in range(y_num):
+                region_detector_path = Path('/data/lxd/jsy/DolphinDetection/data/candidates/test/5/blocks') / (
+                        str(i) + '-' + str(j))
+                detect_params.append(
+                    DetectorParams(x_step, y_step, i, j, self.cfg, region_detector_path))
+        return detect_params
+
+    def detect_frame(self, frame, idx):
+        frame = cv2.GaussianBlur(frame, ksize=(3, 3), sigmaX=0)
+        rects = []
+        for d in self.detect_params:
+            block = DispatchBlock(crop_by_se(frame, d.start, d.end),
+                                  idx, frame.shape)
+            shape = block.frame.shape
+            mask = np.zeros((shape[0], shape[1])).astype(np.uint8)
+            mask[100:900, :] = 255
+            sub_result = detect_mask_task(block.frame, mask, block, d)
+            # sub_result = detect_based_task(block, d)
+            for rect in sub_result.rects:
+                rects.append(rect)
+        return rects
+
+    def detect_video(self, video_path):
+        result_set = []
+        video_capture = cv2.VideoCapture(video_path)
+        ret, frame = video_capture.read()
+        idx = 0
+        while ret:
+            temp = []
+            rects = self.detect_frame(frame, idx)
+            for rect in rects:
+                if rect[2] > 15 and rect[3] > 15 and 100 < rect[1] < 900:
+                    temp.append(rect)
+            if len(temp) > 0:
+                result_set.append((idx, temp))
+            # logger.info(f'idx={idx}, temp={temp}, len_rects={len(rects)}')
+            ret, frame = video_capture.read()
+            idx += 1
+        video_capture.release()
+        return result_set
+
+    def get_median(self, data):
+        if len(data) == 0:
+            return 0
+        data.sort()
+        half = len(data) // 2
+        return (data[half] + data[~half]) / 2
+
+    def get_max_continuous_time(self, data):
+        if len(data) == 0:
+            return 0
+        return max(data)
+
+    def post_filter(self, video_path, task_cnt):
+        """
+        :param video_path: the path of the generated video to be filtered
+        :param task_cnt:
+        :return: False: no fast-object;
+                 True: exists fast-object or float
+        """
+        logger.info(f'Post filter [{self.cfg.index}, {task_cnt}]: started...')
+        if not os.path.exists(video_path):
+            logger.info(f'Post filter [{self.cfg.index}, {task_cnt}]: {video_path} is not exists...')
+            return False
+        result_set = self.detect_video(video_path)
+        if len(result_set) <= 1:
+            return False
+        continuous_time_set = []
+        speed_x_set = []
+        speed_y_set = []
+        continuous_time = 0
+        for i in range(1, len(result_set)):
+            pre_idx, pre_rects = result_set[i - 1]
+            current_idx, current_rects = result_set[i]
+            if len(pre_rects) == len(current_rects):
+                if abs(current_idx - pre_idx) <= 3:
+                    continuous_time += abs(current_idx - pre_idx)
+                else:
+                    continuous_time_set.append(continuous_time)
+                    continuous_time = 0
+                for j in range(len(pre_rects)):
+                    pre_rect = pre_rects[j]
+                    current_rect = current_rects[j]
+                    pre_center_x = pre_rect[0] + pre_rect[2]
+                    pre_center_y = pre_rect[1] + pre_rect[3]
+                    current_center_x = current_rect[0] + current_rect[2]
+                    current_center_y = current_rect[1] + current_rect[3]
+                    speed_x = abs(pre_center_x - current_center_x) / abs(pre_idx - current_idx)
+                    speed_y = abs(pre_center_y - current_center_y) / abs(pre_idx - current_idx)
+                    speed_x_set.append(speed_x)
+                    speed_y_set.append(speed_y)
+                    # logger.info(f'speed_x={speed_x}')
+                    # logger.info(f'speed_y={speed_y}')
+            else:
+                continuous_time_set.append(continuous_time)
+                continuous_time = 0
+        continuous_time_set.append(continuous_time)
+        median_speed_x = self.get_median(speed_x_set)
+        median_speed_y = self.get_median(speed_y_set)
+        max_continuous_time = self.get_max_continuous_time(continuous_time_set)
+        logger.info(
+            f'Post filter [{self.cfg.index}, {task_cnt}]: median_speed_x={median_speed_x},'
+            f' median_speed_y={median_speed_y}, max_continuous_time={max_continuous_time}')
+        if median_speed_x > self.speed_thresh_x or median_speed_y > self.speed_thresh_y:
+            logger.info(
+                f'Post filter [{self.cfg.index}, {task_cnt}]: detect fast-object in [{video_path}]')
+            return True
+        elif max_continuous_time > 20:
+            logger.info(
+                f'Post filter [{self.cfg.index}, {task_cnt}]: detect float in [{video_path}]')
+            return True
+        else:
+            return False
