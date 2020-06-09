f37b410b79f725037d20601d863cc95bbb1cb68a
Shanda Lau <qq313700046@icloud.com>
2020-06-09 23:47:23 +0800

    Down ssd confidence

--- run_1-4.sh
+++ run_1-4.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #
-python app.py --env prod  --enable 31,32,33,34  --enable_forward_filter 31,32,33,34 --use_sm 31,32,33,34  --send_msg --show_windows  31
+python app.py --env prod  --enable 31,32,33,34,35  --enable_forward_filter 31,32,33,34,35 --use_sm 31,32,33,34,35  --send_msg --run_direct
