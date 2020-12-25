77038e563713046bd7315e598506a22fd366553d
Shanda <shandalaulv@gmail.com>
2020-12-25 15:45:12 +0800

    modify jt2 running scripts.

--- run_31-35_direct_jt2.sh
+++ run_31-35_direct_jt2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #
-python app.py --env prod  --enable 41,42,43 --enable_forward_filter 41,42,43,44 --use_sm 41,42,43,44  --run_direct 
+python app.py --env prod  --enable 36 --enable_forward_filter 36,37,38,39,40 --use_sm 36,37,38,39,40  --run_direct 
